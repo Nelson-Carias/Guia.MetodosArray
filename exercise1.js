4697bda11d7342c1e9da40b1ed14728efca65f84
Nelson Carias <Carias030@outlook.com>
2023-10-15 15:51:47 -0600

    Modificacion en el archivo exercise1

--- exercise1.js
+++ exercise1.js
@@ -1,7 +1,9 @@
 // EXERCISE 1
 const numbers = [10, "5", "7", 9, 23, "17"]
 
-const result = numbers.map(x => Number(x)).map((x) => x * 4)
+const result = numbers.map(x => Number(x)).reduce((a, b) => {
+    return a * b
+})
 
 console.log(result)
 
